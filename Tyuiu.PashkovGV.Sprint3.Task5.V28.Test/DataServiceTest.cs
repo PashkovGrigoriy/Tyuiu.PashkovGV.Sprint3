98bea911d81659760c040cb1837edbc53af2ea5c
PashkovGrigoriy <gripas2006@gmail.com>
2025-11-10 18:26:17 +0500

    Task5

---
+++ Tyuiu.PashkovGV.Sprint3.Task5.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.PashkovGV.Sprint3.Task5.V28.Lib;
+namespace Tyuiu.PashkovGV.Sprint3.Task5.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int i = 1;
+            int i2 = 3;
+            int j = 1;
+            int j2 = 12;
+            int x = 2;
+            var res = ds.GetSumSumSeries(x,i,i2,j,j2);
+            Assert.AreEqual(637.775,res);
+        }
+    }
+}
---
+++ Tyuiu.PashkovGV.Sprint3.Task5.V28/Program.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.PashkovGV.Sprint3.Task5.V28.Lib;
+namespace Tyuiu.PashkovGV.Sprint3.Task5.V28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("Введите K: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите K1: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите К2: ");
+            int z = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите K3: ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите К4: ");
+            int j = Convert.ToInt32(Console.ReadLine());
+            double res = ds.GetSumSumSeries(x, y, z,i,j);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
