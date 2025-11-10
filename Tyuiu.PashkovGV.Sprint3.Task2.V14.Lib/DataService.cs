9035b0b0e099f33298c0d70619c19b99d885c5ef
PashkovGrigoriy <gripas2006@gmail.com>
2025-11-10 15:22:19 +0500

    Task2

---
+++ Tyuiu.PashkovGV.Sprint3.Task2.V14.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.PashkovGV.Sprint3.Task2.V14.Lib
+{
+    public class DataService : ISprint3Task2V14
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double res = 1.0;
+            
+            do
+            {
+                res = res * (Math.Pow((value / startValue), 3));
+                startValue++;
+            }
+            while (startValue <= stopValue);
+            return Math.Round(res, 3);
+        }
+    }
+}
+//res = res * (Math.Pow((value/i), 3));
---
+++ Tyuiu.PashkovGV.Sprint3.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PashkovGV.Sprint3.Task2.V14.Lib;
+namespace Tyuiu.PashkovGV.Sprint3.Task2.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int k = 5;
+            int v = 1;
+            int d = 6;
+            var res = ds.GetMultiplySeries(k, v, d);
+            Assert.AreEqual(7.5, res);
+        }
+    }
+}
