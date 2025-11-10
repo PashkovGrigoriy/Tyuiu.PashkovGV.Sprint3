49ac14a34060ddd19172c43aac7b61c5cb4c54ec
PashkovGrigoriy <gripas2006@gmail.com>
2025-11-10 15:57:50 +0500

    Task3

---
+++ Tyuiu.PashkovGV.Sprint3.Task3.V24.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.PashkovGV.Sprint3.Task3.V24.Lib;
+namespace Tyuiu.PashkovGV.Sprint3.Task3.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string str = "sxxrrg x vfrx";
+            char ar = 'x';
+            char str2 =  '2';
+            var res = ds.ReplaceCharInString(str, ar, str2);
+            Assert.AreEqual("s22rrg 2 vfr2", res);
+        }
+    }
+}
---
+++ Tyuiu.PashkovGV.Sprint3.Task3.V24/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.PashkovGV.Sprint3.Task3.V24.Lib;
+namespace Tyuiu.PashkovGV.Sprint3.Task3.V24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            
+            Console.WriteLine("Введите K: ");
+            string x = (Console.ReadLine());
+            Console.WriteLine("Введите K1: ");
+            char y = char.Parse(Console.ReadLine());
+            Console.WriteLine("Введите К2: ");
+            char z = char.Parse(Console.ReadLine());
+            string res = ds.ReplaceCharInString(x, y, z);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+    }
+}
